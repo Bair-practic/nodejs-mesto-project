95cab8ff8bdf17ad784a9f8d7d66c0e28346ad28
bair <badmaiev2002@mail.ru>
2026-01-10 22:47:43 +0300

    part 2

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -1,5 +1,3 @@
-if (!process.env.DATABASE_URL) throw new Error('Ошибка, не найден url подключения к базе данных');
-if (!process.env.SERVER_PORT) throw new Error('Ошибка, не найден порт');
-
-export const { DATABASE_URL } = process.env;
-export const { SERVER_PORT } = process.env;
+export const { DATABASE_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
+export const { SERVER_PORT = 3000 } = process.env;
+export const { JWT_SECRET = 'a13b2c45-9d7e-4c0a-b812-7e4a3d2fbb90' } = process.env;
